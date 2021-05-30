403a43fc3b4dd0ef3f7f134598a811704c979cf5
yoichi <itiyou14@gmail.com>
2021-05-30 16:03:43 +0900

    erbの細かな修正、sign_out後のリダイレクト先を設定m

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,13 +4,18 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if: :devise_controller?
   before_action :authenticate_user!
 
-  def after_sign_in_path_for(resource)
+  def after_sign_in_path_for(_resource)
     books_path
   end
-  def afte_sign_up_path_for(resouce)
+
+  def after_sign_up_path_for(_resouce)
     books_path
   end
-  
+
+  def after_sign_out_path_for(_resouce)
+    new_user_session_path
+  end
+
   protected
 
   def configure_permitted_parameters
