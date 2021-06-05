d18fe89c32332c830ea3dc3ad4e43ad51eaf4670
yoichi <itiyou14@gmail.com>
2021-06-05 15:16:21 +0900

    ユーザー情報update後のリダイレクト方法を変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
   # routesは上から読まれる
 
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations' }
   resources :users, only: [:show, :index]
   resources :books
-  as :user do
-    get 'books', :to => 'books#index', :as => :user_root
-  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
