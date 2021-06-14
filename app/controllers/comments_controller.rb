b9abcfb876d57d53921665099d3c158c8ead3d85
yoichi <itiyou14@gmail.com>
2021-06-15 07:26:57 +0900

    commentの削除機能実装、rubocopの適用

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,62 +1,36 @@
+# frozen_string_literal: true
+
 class CommentsController < ApplicationController
-  before_action :set_comment, only: %i[ show edit update destroy ]
+  before_action :set_comment, only: %i[destroy]
   before_action :set_commentable
-
-  # GET /comments or /comments.json
-  def index
-    @comments = @commentable.comments
-  end
-
-  # GET /comments/1 or /comments/1.json
-  def show
-  end
-
-  # GET /comments/new
-  def new
-     @comment = @commentable.comments.build
-  end
-
-  # GET /comments/1/edit
-  def edit
-  end
 
   def create
     @comment = @commentable.comments.build(comment_params)
     @comment.user = current_user
     if @comment.save
-      redirect_to @commentable, notice: 'Comment was successfully created.'
+      redirect_to @commentable, notice: t(:notice_create, scope: 'controllers.common', name: Comment.model_name.human)
     else
       render :new
     end
   end
 
-  # PATCH/PUT /comments/1 or /comments/1.json
-  def update
-    if @comment.update(comment_params)
-      redirect_to [@commentable, @comment], notice: 'Comment was successfully updated.'
-    else
-      render :edit
-    end
-  end
-
-  # DELETE /comments/1 or /comments/1.json
   def destroy
     @comment.destroy
-    redirect_to [@commentable, :comments], notice: 'Comment was successfully destroyed.'
+    redirect_to @commentable, notice: t(:notice_destroy, scope: 'controllers.common', name: Comment.model_name.human)
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_comment
-      @comment = Comment.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def comment_params
-      params.require(:comment).permit(:body)
-    end
-    def set_commentable
-      resource, id = request.path.split('/')[1,2]
-      @commentable = resource.singularize.classify.constantize.find(id)
-    end
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body)
+  end
+
+  def set_commentable
+    resource, id = request.path.split('/')[1, 2]
+    @commentable = resource.singularize.classify.constantize.find(id)
+  end
 end
--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,15 +1,16 @@
+# frozen_string_literal: true
+
 class ReportsController < ApplicationController
-  before_action :set_report, only: %i[ show edit update destroy ]
+  before_action :set_report, only: %i[show edit update destroy]
   before_action :correct_user, only: %i[edit update destroy]
   before_action :unique_report_at_date_for_each_user, only: %i[create]
   # GET /reports or /reports.json
   def index
-    @reports = Report.all.includes(:user).order(id: "DESC").page(params[:page])
+    @reports = Report.all.includes(:user).order(id: 'DESC').page(params[:page])
   end
 
   # GET /reports/1
-  def show
-  end
+  def show; end
 
   # GET /reports/new
   def new
@@ -17,15 +18,14 @@ class ReportsController < ApplicationController
   end
 
   # GET /reports/1/edit
-  def edit
-  end
+  def edit; end
 
   # POST /reports
   def create
     @report = current_user.reports.new(report_params)
     respond_to do |format|
       if @report.save
-        format.html { redirect_to @report, notice: t(:notice_create,scope:'controllers.common' ,name: Report.model_name.human) }
+        format.html { redirect_to @report, notice: t(:notice_create, scope: 'controllers.common', name: Report.model_name.human) }
       else
         format.html { render :new, status: :unprocessable_entity }
       end
@@ -36,7 +36,7 @@ class ReportsController < ApplicationController
   def update
     respond_to do |format|
       if @report.update(report_params)
-        format.html { redirect_to @report, notice:  t(:notice_update,scope:'controllers.common' ,name: Report.model_name.human) }
+        format.html { redirect_to @report, notice: t(:notice_update, scope: 'controllers.common', name: Report.model_name.human) }
       else
         format.html { render :edit, status: :unprocessable_entity }
       end
@@ -47,31 +47,32 @@ class ReportsController < ApplicationController
   def destroy
     @report.destroy
     respond_to do |format|
-      format.html { redirect_to reports_url, notice:  t(:notice_destroy,scope:'controllers.common' ,name: Report.model_name.human) }
+      format.html { redirect_to reports_url, notice: t(:notice_destroy, scope: 'controllers.common', name: Report.model_name.human) }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_report
-      @report = Report.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def report_params
-      params.require(:report).permit(:title, :content, :user_id)
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_report
+    @report = Report.find(params[:id])
+  end
 
-    def correct_user
-      report = Report.find(params[:id])
-      redirect_to(reports_path) unless report.user == current_user
-    end
+  # Only allow a list of trusted parameters through.
+  def report_params
+    params.require(:report).permit(:title, :content, :user_id)
+  end
 
-    def unique_report_at_date_for_each_user
-      user = User.find(current_user.id)
-      reports = user.reports.all.order(id: :ASC)
-      if reports.map{|report| report.created_at.to_date}.include?(DateTime.now.to_date)
-        redirect_to(edit_report_path(reports.last.id) , notice: t(:notice_created_today, scope:'controllers.common' , name: Report.model_name.human))
-      end
-    end
+  def correct_user
+    report = Report.find(params[:id])
+    redirect_to(reports_path) unless report.user == current_user
+  end
+
+  def unique_report_at_date_for_each_user
+    user = User.find(current_user.id)
+    reports = user.reports.all.order(id: :ASC)
+    return unless reports.map { |report| report.created_at.to_date }.include?(DateTime.now.to_date)
+
+    redirect_to(edit_report_path(reports.last.id), notice: t(:notice_created_today, scope: 'controllers.common', name: Report.model_name.human))
+  end
 end
