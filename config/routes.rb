2d6cd0023cfda36602b6262273d40d3270a6445e
yoichi <itiyou14@gmail.com>
2021-06-21 18:36:07 +0900

    Commentのアクション指定を追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :reports do
-    resources :comments
+    resources :comments, only: %i[create destroy]
   end
   mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
   devise_for :users
